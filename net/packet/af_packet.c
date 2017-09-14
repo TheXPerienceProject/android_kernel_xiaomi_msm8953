923a24e929ed913190c22af17c4b4a9cb96a97c6
Willem de Bruijn <willemb@google.com>
2017-09-14 17:14:41 -0400

    packet: hold bind lock when rebinding to fanout hook
    [ Upstream commit 008ba2a13f2d04c947adc536d19debb8fe66f110 ]
    
    Packet socket bind operations must hold the po->bind_lock. This keeps
    po->running consistent with whether the socket is actually on a ptype
    list to receive packets.
    
    fanout_add unbinds a socket and its packet_rcv/tpacket_rcv call, then
    binds the fanout object to receive through packet_rcv_fanout.
    
    Make it hold the po->bind_lock when testing po->running and rebinding.
    Else, it can race with other rebind operations, such as that in
    packet_set_ring from packet_rcv to tpacket_rcv. Concurrent updates
    can result in a socket being added to a fanout group twice, causing
    use-after-free KASAN bug reports, among others.
    
    Reported independently by both trinity and syzkaller.
    Verified that the syzkaller reproducer passes after this patch.
    
    Fixes: dc99f600698d ("packet: Add fanout support.")
    Reported-by: nixioaming <nixiaoming@huawei.com>
    Signed-off-by: Willem de Bruijn <willemb@google.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    Signed-off-by: klozz <klozz707@gmail.com>

--- net/packet/af_packet.c
+++ net/packet/af_packet.c
@@ -1433,6 +1433,7 @@ static int fanout_add(struct sock *sk, u16 id, u16 type_flags)
 		return -EINVAL;
 	}
 
+<<<<<<< HEAD
 	mutex_lock(&fanout_mutex);
 
 	err = -EINVAL;
@@ -1440,6 +1441,8 @@ static int fanout_add(struct sock *sk, u16 id, u16 type_flags)
 		goto out;
 
 	err = -EALREADY;
+=======
+>>>>>>> e4ffdf9... packet: hold bind lock when rebinding to fanout hook
 	if (po->fanout)
 		goto out;
 
